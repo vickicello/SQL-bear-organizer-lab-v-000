fd540d2fa9695ed75207b4fa6ca6f9a73f7c99b4
Vicki Hamm <vicki.cello@gmail.com>
2018-08-03 19:12:02 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "brown", "nice", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 8, "F", "black", "hyper", 1);
