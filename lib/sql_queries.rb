db34d006c5a00604b470c82b12c0d661ba270bcb
Vicki Hamm <vicki.cello@gmail.com>
2018-08-03 19:34:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY color DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY color DESC LIMIT 1;"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
